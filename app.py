f0c63f47cce2c24865fbedb340ae9465181174e9
Felipe Flores <felipeguilhermefs@gmail.com>
2022-11-11 17:21:11 +0000

    New redis instance

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.logger.setLevel(logging.INFO)
 
 def get_redis():
     if not hasattr(g, 'redis'):
-        g.redis = Redis(host="redis", db=0, socket_timeout=5)
+        g.redis = Redis(host="redis-lion.redis.cache.windows.net", db=0, socket_timeout=5, password="r6dUPrtN0P0GEMaG8uubT86G6awolLIs4AzCaPCSqVI=")
     return g.redis
 
 @app.route("/", methods=['POST','GET'])
